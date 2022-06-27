3f0b02eaf77d43e6d0ed3482e5b30b35f29d099d
Muyao Feng <v-muyaofeng@microsoft.com>
2022-06-27 17:36:21 +0800

    change for comments

--- src/test/java/com/azure/spring/dev/tools/dependency/support/SpringBootReleaseNotesReaderTest.java
+++ src/test/java/com/azure/spring/dev/tools/dependency/support/SpringBootReleaseNotesReaderTest.java
@@ -8,18 +8,18 @@ import org.junit.jupiter.api.Test;
 
 class SpringBootReleaseNotesReaderTest {
     private final String releaseNotesUrl = "https://github.com/spring-projects/spring-boot/releases/tag/v2.7.0";
-    private final String htmlContents = "<h2>\n Bug Fixes\n <ul>\n <li>\n Contributors \n </li>\n";
     private final SpringBootReleaseNotesReader springBootReleaseNotesReader =
         new SpringBootReleaseNotesReader(null, null);
 
     @Test
     void testConvertOutputString() {
+        String htmlContents = "<h2>\n Bug Fixes\n <ul>\n <li>\n Contributors \n </li>\n";
         String releaseNotesContents = springBootReleaseNotesReader.convertOutputString(releaseNotesUrl, htmlContents);
 
         Assertions.assertEquals(1, releaseNotesContents.trim().split("\n").length);
-        Assertions.assertEquals(-1,releaseNotesContents.indexOf("h2"));
-        Assertions.assertEquals(-1,releaseNotesContents.indexOf("Contributors"));
-        Assertions.assertNotEquals(-1,releaseNotesContents.indexOf(releaseNotesUrl));
+        Assertions.assertEquals(-1, releaseNotesContents.indexOf("h2"));
+        Assertions.assertEquals(-1, releaseNotesContents.indexOf("Contributors"));
+        Assertions.assertNotEquals(-1, releaseNotesContents.indexOf(releaseNotesUrl));
         Assertions.assertTrue(releaseNotesContents.startsWith("<details><summary>Release notes</summary>"));
         Assertions.assertTrue(releaseNotesContents.endsWith("</details>"));
     }
