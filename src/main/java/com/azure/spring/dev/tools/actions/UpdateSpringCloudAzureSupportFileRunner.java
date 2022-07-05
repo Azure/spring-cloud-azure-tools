10084ca2a620dd33154273e43dabe6bb639d4a34
Muyao Feng <92105726+Netyyyy@users.noreply.github.com>
2022-07-05 10:30:16 +0800

    Update action of update-spring-cloud-azure-support-file (#13)
    * not to update end of life versions
    
    * update PR description

--- src/main/java/com/azure/spring/dev/tools/actions/UpdateSpringCloudAzureSupportFileRunner.java
+++ src/main/java/com/azure/spring/dev/tools/actions/UpdateSpringCloudAzureSupportFileRunner.java
@@ -25,6 +25,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -41,7 +42,7 @@ public class UpdateSpringCloudAzureSupportFileRunner implements CommandLineRunne
     private static final Logger LOGGER = LoggerFactory.getLogger(UpdateSpringCloudAzureSupportFileRunner.class);
     private final SpringProjectMetadataReader springProjectMetadataReader;
     private final Map<String, VersionRange> springCloudCompatibleSpringBootVersionRanges;
-    private final SpringCloudAzureSupportMetadataReader azureSupportMetadataReader;
+    private final Map<String, SpringCloudAzureSupportMetadata> azureSupportMetadataMap;
     private final ObjectMapper objectMapper;
 
     public UpdateSpringCloudAzureSupportFileRunner(SpringProjectMetadataReader springProjectMetadataReader,
@@ -49,16 +50,16 @@ public class UpdateSpringCloudAzureSupportFileRunner implements CommandLineRunne
                                                    SpringCloudAzureSupportMetadataReader azureSupportMetadataReader,
                                                    ObjectMapper objectMapper) {
         this.springProjectMetadataReader = springProjectMetadataReader;
-        this.azureSupportMetadataReader = azureSupportMetadataReader;
         this.objectMapper = objectMapper;
         this.springCloudCompatibleSpringBootVersionRanges =
             springInitializrMetadataReader.getCompatibleSpringBootVersions("spring-cloud");
+        this.azureSupportMetadataMap = azureSupportMetadataReader.getAzureSupportMetadata().stream().collect(
+            Collectors.toMap(SpringCloudAzureSupportMetadata::getSpringBootVersion, Function.identity()));
     }
 
     @Override
     public void run(String... args) throws Exception {
         LOGGER.info("---------- starting {} ----------", UpdateSpringCloudAzureSupportFileRunner.class.getSimpleName());
-        List<SpringCloudAzureSupportMetadata> azureSupportMetadata = azureSupportMetadataReader.getAzureSupportMetadata();
 
         final Set<String> activeSpringBootVersions = new HashSet<>();
 
@@ -68,30 +69,32 @@ public class UpdateSpringCloudAzureSupportFileRunner implements CommandLineRunne
             .map(CONVERTER::convert)
             .filter(Objects::nonNull)
             .peek(s -> s.setSpringCloudVersion(findCompatibleSpringCloudVersion(s.getSpringBootVersion())))
-            .peek(s -> s.setSupportStatus(
-                findSupportStatus(azureSupportMetadata, s.getSpringBootVersion())))
+            .peek(s -> s.setSupportStatus(findSupportStatus(s.getSpringBootVersion())))
             .peek(s -> activeSpringBootVersions.add(s.getSpringBootVersion()))
             .collect(Collectors.toList());
 
-        List<SpringCloudAzureSupportMetadata> snapshot = azureSupportMetadata
+        List<SpringCloudAzureSupportMetadata> snapshot = azureSupportMetadataMap
+            .values()
             .stream()
             .filter(s -> !activeSpringBootVersions.contains(s.getSpringBootVersion()))
             .peek(s -> s.setCurrent(false))
             .peek(s -> s.setSupportStatus(SupportStatus.END_OF_LIFE))
             .collect(Collectors.toList());
 
-        List<SpringCloudAzureSupportMetadata> result = Stream.concat(current.stream(), snapshot.stream())
-                                                             .sorted((o1, o2) -> {
-            Version v1 = Version.parse(o1.getSpringBootVersion());
-            Version v2 = Version.parse(o2.getSpringBootVersion());
-            return v2.compareTo(v1);
-        }).collect(Collectors.toList());
+        List<SpringCloudAzureSupportMetadata> result = Stream
+            .concat(current.stream(), snapshot.stream())
+            .sorted((o1, o2) -> {
+                Version v1 = Version.parse(o1.getSpringBootVersion());
+                Version v2 = Version.parse(o2.getSpringBootVersion());
+                return v2.compareTo(v1);
+            }).collect(Collectors.toList());
 
         writeToFile(result);
     }
 
     private void writeToFile(List<SpringCloudAzureSupportMetadata> result) throws IOException {
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("spring-cloud-azure-supported-spring.json"))) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(
+            new FileWriter("spring-cloud-azure-supported-spring.json"))) {
             objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
 
             DefaultPrettyPrinter prettyPrinter = new DefaultPrettyPrinter();
@@ -102,17 +105,16 @@ public class UpdateSpringCloudAzureSupportFileRunner implements CommandLineRunne
         }
     }
 
-    private SupportStatus findSupportStatus(List<SpringCloudAzureSupportMetadata> azureSupportStatus,
-                                            String springBootVersion) {
-        for (SpringCloudAzureSupportMetadata azureSupportMetadata : azureSupportStatus) {
-            if (springBootVersion.equalsIgnoreCase(azureSupportMetadata.getSpringBootVersion())) {
-                return azureSupportMetadata.getSupportStatus();
-            }
-        }
-        return null;
+    private SupportStatus findSupportStatus(String springBootVersion) {
+        SpringCloudAzureSupportMetadata supportMetadata = this.azureSupportMetadataMap.get(springBootVersion);
+        return supportMetadata == null ? null : supportMetadata.getSupportStatus();
     }
 
     private String findCompatibleSpringCloudVersion(String springBootVersion) {
+        SpringCloudAzureSupportMetadata supportMetadata = this.azureSupportMetadataMap.get(springBootVersion);
+        if (supportMetadata != null && supportMetadata.getSupportStatus() == SupportStatus.END_OF_LIFE) {
+            return supportMetadata.getSpringCloudVersion();
+        }
         return this.springCloudCompatibleSpringBootVersionRanges
             .entrySet()
             .stream()
